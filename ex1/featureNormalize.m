1520c69e725950e8429e8d80810ff929dceff7cc
Larissa Lages <llo@cin.ufpe.br>
2019-02-19 23:12:06 -0500

    normalize the features (fix)

--- ex1/featureNormalize.m
+++ ex1/featureNormalize.m
@@ -8,13 +8,14 @@ function [X_norm, mu, sigma] = featureNormalize(X)
 % You need to set these values correctly
 X_norm = X;
 mu = zeros(1, size(X, 2));
-sigma = zeros(1, size(X, 2));
-       
-for i = 1:size(X,2)
-   mu(i) = mean(X(:,i));
-   sigma(i) = std(X(:,1));
+sigma = zeros(1, size(X, 2));       
+
+for i = 1:size(X, 2)
+	mu(:,i) = mean(X_norm(:,i));
+	sigma(:,i) = std(X_norm(:,i));
+	X_norm(:,i) = (X_norm(:,i) - mu(:,i)) / sigma(:,i);
 end
 
-
+% ============================================================
 
 end
