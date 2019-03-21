6aee02ec9de74277e71b157d51c6392582d4ad35
Larissa Lages <llo@cin.ufpe.br>
2019-03-20 22:01:23 -0400

    choosing C and sigma

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -11,24 +11,34 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 C = 1;
 sigma = 0.3;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return the optimal C and sigma
-%               learning parameters found using the cross validation set.
-%               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
-%                   predictions = svmPredict(model, Xval);
-%               will return the predictions on the cross validation set.
-%
-%  Note: You can compute the prediction error using 
-%        mean(double(predictions ~= yval))
-%
+C_values = [0.01, 0,03, 0.1, 0.3, 1, 3, 10, 30];
+sigma_values = [0.01, 0,03, 0.1, 0.3, 1, 3, 10, 30];
+small_error = Inf;
+
+
+for i=1:length(C_values)
+    for j=1:length(sigma_values)
+        
+        C_test = C_values(i);
+        sigma_test = sigma_values(j);
+        model = svmTrain(X, y, C_test, @(x1, x2)gaussianKernel(x1, x2, sigma_test));
+        predictions = svmPredict(model, Xval);
+        error = mean(double(predictions ~= yval));
+        
+        if (error < small_error)
+            C = C_test;
+            sigma = sigma_test;
+            small_error = error;
+        end
+        
+    end
+    
+end
 
 
 
-
-
-
+end
 
 % =========================================================================
 
-end
+
